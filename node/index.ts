491d19f03fb486dd2698238671259c9aac9cad5f
Claudiu Ionita <73105476+claudiu-iviteb@users.noreply.github.com>
2025-03-26 13:52:56 +0200

    cr feedback

--- node/index.ts
+++ node/index.ts
@@ -1,16 +1,27 @@
 import type {
+  Cached,
   ClientsConfig,
   ParamsContext,
   RecorderState,
   ServiceContext,
 } from '@vtex/api'
-import { Service, method } from '@vtex/api'
+import { LRUCache, Service, method } from '@vtex/api'
 
 import { Clients } from './clients'
-import ping from './middlewares/ping'
 import { getProductRating } from './resolvers/getProductRating'
+import ping from './middlewares/ping'
 
-const TIMEOUT_MS = 1000
+const TIMEOUT_MS = 3 * 1000
+const TWO_HOURS_MS = 120 * 60 * 1000
+const MAX_SIZE_FOR_CACHE = 20000
+
+const yotpoCache = new LRUCache<string, Cached>({
+  max: MAX_SIZE_FOR_CACHE,
+  maxAge: TWO_HOURS_MS,
+  ttl: TWO_HOURS_MS,
+})
+
+metrics.trackCache('yotpo', yotpoCache)
 
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
@@ -18,6 +29,11 @@ const clients: ClientsConfig<Clients> = {
     default: {
       retries: 2,
       timeout: TIMEOUT_MS,
+    },
+    yotpo: {
+      retries: 1,
+      concurrency: 10,
+      memoryCache: yotpoCache,
     },
   },
 }
